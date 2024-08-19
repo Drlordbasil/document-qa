917afddc6f92b20799e4256296160cb2da809668
Lord Basil - Automate EVERYTHING <126736516+Drlordbasil@users.noreply.github.com>
2024-08-18 20:11:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 📄 Document question answering template
+
+A simple Streamlit app that answers questions about an uploaded document via OpenAI's GPT-3.5.
+
+[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://document-question-answering-template.streamlit.app/)
+
+### How to run it on your own machine
+
+1. Install the requirements
+
+   ```
+   $ pip install -r requirements.txt
+   ```
+
+2. Run the app
+
+   ```
+   $ streamlit run streamlit_app.py
+   ```
---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.title("📄 Document question answering")
+st.write(
+    "Upload a document below and ask a question about it – GPT will answer! "
+    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+)
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via `st.text_area`.
+    question = st.text_area(
+        "Now ask a question about the document!",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using `st.write_stream`.
+        st.write_stream(stream)
